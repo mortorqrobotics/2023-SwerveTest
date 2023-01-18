d4dea5b61ac8cbac8c230ecc283cc11bda7afc55
Hayley <96212232+HayleyIsCool@users.noreply.github.com>
2023-01-17 16:47:56 -0800

    initial commit

---
+++ src/main/java/org/team1515/SwerveTest/Controls.java
@@ -0,0 +1,27 @@
+package org.team1515.SwerveTest;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class Controls {
+    // change botton maps later
+
+    public static final Trigger ALIGN = new Trigger(RobotContainer.mainController::getStartButton); // will limelight
+                                                                                                    // and apriltag
+                                                                                                    // overlap?
+    public static final Trigger RESET_GYRO = new Trigger(RobotContainer.mainController::getBackButton);
+    public static final BooleanSupplier DRIVE_ROBOT_ORIENTED = () -> RobotContainer.mainController.getLeftBumper();
+
+    public static boolean getRightTrigger() {
+        return RobotContainer.mainController.getRightTriggerAxis() >= 0.250;
+    }
+
+    public static boolean getLeftTrigger() {
+        return RobotContainer.mainController.getLeftTriggerAxis() >= 0.250;
+    }
+
+    public static boolean secondRightStickTrigger() {
+        return RobotContainer.secondController.getRightTriggerAxis() >= 0.250;
+    }
+}
---
+++ src/main/java/org/team1515/SwerveTest/RobotContainer.java
@@ -0,0 +1,86 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team1515.SwerveTest;
+
+import org.team1515.SwerveTest.SwerveCommand;
+
+import org.team1515.SwerveTest.Swerve;
+import org.team1515.SwerveTest.Controls;
+
+import com.team364.swervelib.util.SwerveConstants;
+
+import java.util.function.BooleanSupplier;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class RobotContainer {
+  public static XboxController mainController;
+  public static XboxController secondController;
+
+  public static Swerve drivetrain;
+  public static Gyroscope gyro;
+
+  public RobotContainer() {
+    mainController = new XboxController(0);
+
+    drivetrain = new Swerve();
+
+    gyro = new Gyroscope();
+
+    configureBindings();
+  }
+
+  private void configureBindings() {
+
+    drivetrain.setDefaultCommand(
+        new SwerveCommand(drivetrain,
+            () -> -modifyAxis(-mainController.getLeftY() * getRobotSpeed()) * SwerveConstants.Swerve.maxSpeed,
+            () -> -modifyAxis(-mainController.getLeftX() * getRobotSpeed()) * SwerveConstants.Swerve.maxSpeed,
+            () -> -modifyAxis(mainController.getRightX() * getRobotSpeed()) * SwerveConstants.Swerve.maxAngularVelocity,
+            () -> Controls.DRIVE_ROBOT_ORIENTED.getAsBoolean()));
+
+    Controls.RESET_GYRO.onTrue(new InstantCommand(() -> drivetrain.zeroGyro())); // drivetrain::zeroGyro not working
+
+    // Controls.ALIGN.onTrue(new Align(drivetrain));
+
+  }
+
+  // public Command getAutonomousCommand() {
+  // return new AutoCommandScore(drivetrain);
+  // }
+
+  public static double getRobotSpeed() {
+    return Controls.getLeftTrigger() ? 0.45 : 0.7;
+    // return 0.7;
+  }
+
+  public static double modifyAxis(double value) {
+    value = deadband(value, 0.08);
+
+    // Square the axis
+    value = Math.copySign(value * value, value);
+
+    return value;
+  }
+
+  public static double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
+
+}
